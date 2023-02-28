646315745d8d9b260101de581fa53dd8f17c327f
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-02-28 10:30:18 +0000

    Added Previous entries string

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 let countEl = document.querySelector("#count-el");
+let saveEl = document.querySelector("#save-el");
 let count = 0;
 
 function increment() {
-  count = ++count;
+  count += 1;
   countEl.innerText = count;
-  console.log(count);
+}
+
+function save() {
+  let countStr = " " + count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
 
 let incrementBtn = document.querySelector("#increment-btn");
 incrementBtn.addEventListener("click", increment);
+
+let saveBtn = document.querySelector("#save-btn");
+saveBtn.addEventListener("click", save);
